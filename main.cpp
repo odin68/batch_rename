bb3fbfba1e5883f941a3fc02f7059e7527619f84
Odin <deepsixbc@gmail.com>
2024-04-10 13:55:54 -0700

    Setup command line options for use case 1 (Pictures)

--- main.cpp
+++ main.cpp
@@ -11,15 +11,47 @@ int main(int argc, char* argv[])
     po::options_description desc("Allowed options");
     
     // Bind variables for program options
-    int n0 = 0;
+    string c0;
+    string c1;
+    string c2;
+    string c3;
+    string c4;
+    string c5;
+    string c6;
+    string c7;
+    string c8;
+    string c9;
+    string extension;
+    string format_string;
 
     try
     {
         // Declare supported options
         desc.add_options()
-            ("first_counter,n0",
-                po::value<int>(&n0)->default_value(10),
-                "set the first counter to a starting value. Use leading zeros for padding.")
+            ("format_string,f",
+                po::value<string>(&format_string), "set a new file extension.")
+            ("counter_0,0",
+                po::value<string>(&c0), "set the first counter to a starting value (use leading zeros for padding).")
+            ("counter_1,1",
+                po::value<string>(&c1), "set the second counter to a starting value (use leading zeros for padding).")
+            ("counter_2,2",
+                po::value<string>(&c2), "set the third counter to a starting value (use leading zeros for padding).")
+            ("counter_3,3",
+                po::value<string>(&c3), "set the fourth counter to a starting value (use leading zeros for padding).")
+            ("counter_4,4",
+                po::value<string>(&c4), "set the fifth counter to a starting value (use leading zeros for padding).")
+            ("counter_5,5",
+                po::value<string>(&c5), "set the sixth counter to a starting value (use leading zeros for padding).")
+            ("counter_6,6",
+                po::value<string>(&c6), "set the seventh counter to a starting value (use leading zeros for padding).")
+            ("counter_7,7",
+                po::value<string>(&c7), "set the eighth counter to a starting value (use leading zeros for padding).")
+            ("counter_8,8",
+                po::value<string>(&c8), "set the nineth counter to a starting value (use leading zeros for padding).")
+            ("counter_9,9",
+                po::value<string>(&c9), "set the tenth counter to a starting value (use leading zeros for padding).")
+            ("extension,e",
+                po::value<string>(&extension), "set a new file extension.")
             ;
 
         // Store the options in a variables_map
@@ -27,9 +59,53 @@ int main(int argc, char* argv[])
         po::store(po::parse_command_line(argc, argv, desc), vm);
         po::notify(vm);
 
-        // Option with a bind variable and default value
-        if (vm.count("first_counter"))
-            cout << "first_counter was set to " << n0 << endl;
+        // Format string
+        if (vm.count("format_string"))
+            cout << "format_string was set to " << format_string << endl;
+
+        // First counter
+        if (vm.count("counter_0"))
+            cout << "counter_0 was set to " << c0 << endl;
+
+        // First counter
+        if (vm.count("counter_1"))
+            cout << "counter_1 was set to " << c1 << endl;
+
+        // First counter
+        if (vm.count("counter_2"))
+            cout << "counter_2 was set to " << c2 << endl;
+
+        // First counter
+        if (vm.count("counter_3"))
+            cout << "counter_3 was set to " << c3 << endl;
+
+        // First counter
+        if (vm.count("counter_4"))
+            cout << "counter_4 was set to " << c4 << endl;
+
+        // First counter
+        if (vm.count("counter_5"))
+            cout << "counter_5 was set to " << c5 << endl;
+
+        // First counter
+        if (vm.count("counter_6"))
+            cout << "counter_6 was set to " << c6 << endl;
+
+        // First counter
+        if (vm.count("counter_7"))
+            cout << "counter_7 was set to " << c7 << endl;
+
+        // First counter
+        if (vm.count("counter_8"))
+            cout << "counter_8 was set to " << c8 << endl;
+
+        // First counter
+        if (vm.count("counter_9"))
+            cout << "counter_9 was set to " << c9 << endl;
+
+        // Extension
+        if (vm.count("extension"))
+            cout << "extension was set to " << extension << endl;
     }
     catch (exception& e)
     {
--- readme.md
+++ readme.md
@@ -1,21 +1,22 @@
-batch_rename
-------------
+# batch_rename
 
-Use Case 1:
-	Rename picture files
-	batch_rename -f "/d-/n0" -n0 001 -e "jpg" *.jpeg
-	-f = format string
-		/d = use directory name
-		/n0 = use first counter
-	-n0 = define first counter
-	-e = define new file extension
-
+## Use Case 1: Rename picture files
+### Switches
+```
+batch_rename -f "/d-/0" -0 001 -e "jpg" *.jpeg
+```
+> -f&emsp;= format string
+> &emsp;/d&ensp;&nbsp;= use directory name
+> &emsp;/0&nbsp;= use first counter
+> -0&nbsp;= define first counter using leading zeros as padding
+> -e&ensp;&nbsp;= define new file extension
+### Example
 	Result for file "file.jpeg" in directory "dir"
 		batch_rename -f "/d-/n0" -n0 001 -e "jpg" *.jpeg
 		file.jpeg -> dir-001.jpg
 
-Use Case 2:
-	Rename music files
+## Use Case 2: Rename music files
+### Switches
 	batch_rename -is "-" -i "1,2" -f "/n0-/i0-/i1-[live]" -n0 01 .flac
 	-is = define the input separator
 	-i = pick which tokens to define (first becomes i0, second becomes i1, etc.)
@@ -24,29 +25,32 @@ Use Case 2:
 		/i0 = use first input
 		/i1 = use second input
 	-n0 = define first counter
-
+### Example
 	Result for file "1_I like cheese_[reprise].flac"
 		batch_rename -is "_" -i "1,2" -f "/n0-/i1-/i2" -n0 01 *.flac
 		1_I like cheese_[reprise].flac -> 01-I like cheese-[reprise].flac
 
-Use Case 3:
-	Rename video files make file like Video-S01E02.avi
+## Use Case 3: Rename video files make file like Video-S01E02.avi
+### Switches
 	batch_rename -f "/d-S01E/n1" -n1 01 *.mkv
 	-no new switches
-
+### Example
 	Result for file "first episide of south park.mkv" in directory "South Park"
 		batch_rename -f"/d-S01E/n0" -n0 01 *.mkv
 		first episide of south park.mkv -> South Park-S01E01.mkv
 
-replace_char
+## replace_char
+### Switches
 	batch_rename -rc ".&/ " *.jpg
 	-rc = define replace char. In this case . AND & becomes " " (space)
 
-replace_string
+## replace_string
+### Switches
 	batch_rename -rc ".&/ " *.jpg
 	-rs = define replace string. In this case .& becomes " " (space)
 
-insert_string
+## insert_string
+### Switches
 	batch_rename -in " /@A" *.jpg
 	-in = define insert string. In this case " " (space) is inserted before any upper case character (@A)
 
